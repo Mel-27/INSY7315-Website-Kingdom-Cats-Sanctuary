82c50b71e238e03b055e22e27d383fbf34835b4a
Mel-27 <ST10438899@vcconnect.edu.za>
2026-07-24 11:17:32 +0200

    This is the first commit to the website repo, I created the User model for the login and register

---
+++ INSY7315Prototype/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace INSY7315Prototype.Models
+{
+    public class User
+    {
+        [Key] 
+        public int userId { get; set; }
+    
+        [Required]
+        public string fullName { get; set; }   = string.Empty;
+
+
+        [Required]
+        public string email { get; set; }
+
+        [Required]
+        public string password { get; set; }
+      
+
+    }
+}
