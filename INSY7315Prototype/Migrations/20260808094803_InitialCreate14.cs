81f84cc328ce07aac4b90a5dcc7c0f5034e938dd
Mel-27 <ST10438899@vcconnect.edu.za>
2026-08-10 11:47:18 +0200

    In this commit, I changed the cats status and badge status and then did a migration because I made changes to the appDB context

---
+++ INSY7315Prototype/Migrations/20260808094803_InitialCreate14.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace INSY7315Prototype.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate14 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Cats",
+                keyColumn: "Id",
+                keyValue: 40,
+                columns: new[] { "Badge", "Description", "TagsCsv" },
+                values: new object[] { "New Arrival", "Binx is very loving and friendly, he thrives best in a calm, dedicated indoor home. He's gentle and deeply grateful for the extra care and patience of the right adopter.", "Gentle,Indoor Only" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Cats",
+                keyColumn: "Id",
+                keyValue: 40,
+                columns: new[] { "Badge", "Description", "TagsCsv" },
+                values: new object[] { "Special Needs", "Binx has some special needs that mean he thrives best in a calm, dedicated indoor home. He's gentle and deeply grateful for the extra care and patience of the right adopter.", "Gentle,Special Needs,Indoor Only" });
+        }
+    }
+}
